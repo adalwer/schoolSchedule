a1949220a8cd9d573e8f105ec087318d42cbb8bf
Adam Furtak <adas.furtak@gmail.com>
2017-03-02 09:41:34 +0100

    dodanie przyciskow z funkcja obslugujaca je

--- schoolSchedule/mainwindow.h
+++ schoolSchedule/mainwindow.h
@@ -2,21 +2,25 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-
-namespace Ui {
-class MainWindow;
-}
+#include <QPushButton>
+#include <QSignalMapper>
 
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
-
 public:
-    explicit MainWindow(QWidget *parent = 0);
-    ~MainWindow();
+    MainWindow ();
 
 private:
-    Ui::MainWindow *ui;
+    int lesson_width, lesson_height;
+    QPushButton *lesson [5][8];
+    QSignalMapper *signalMapper;
+
+    void set_initial_values ();
+    void createUI ();
+private slots:
+    void fun (int buttonId);
+
 };
 
 #endif // MAINWINDOW_H
